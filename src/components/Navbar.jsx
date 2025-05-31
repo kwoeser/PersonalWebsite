87dd0baaf01e91a4a6889cf4612baf7a9079a555
kwoeser <karmawoeser1@gmail.com>
2025-05-31 13:49:54 -0700

    fixed res

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,32 @@
 import { Link } from "react-router-dom";
 import "../index.css"; 
 import { useState } from "react";
-import { Sun, Moon } from 'lucide-react';
+import { Sun, Moon, Menu, X } from 'lucide-react';
 
 function Navbar() {
   const [lightMode, setLightMode] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleTheme = () => {
     setLightMode(!lightMode);
   };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="nav-links">
-        <Link to="/" className="nav-item">home</Link>
-        <Link to="/Projects" className="nav-item">projects</Link>
-        <Link to="/Favorites" className="nav-item">favorites</Link>
-        <Link to="/Contact" className="nav-item">contact</Link>
+      <div className={`nav-links ${isMobileMenuOpen ? "mobile-menu-open" : ""}`}>
+        <Link to="/" className="nav-item" onClick={isMobileMenuOpen ? toggleMobileMenu : undefined}>home</Link>
+        <Link to="/Projects" className="nav-item" onClick={isMobileMenuOpen ? toggleMobileMenu : undefined}>projects</Link>
+        <Link to="/Favorites" className="nav-item" onClick={isMobileMenuOpen ? toggleMobileMenu : undefined}>favorites</Link>
+        <Link to="/Contact" className="nav-item" onClick={isMobileMenuOpen ? toggleMobileMenu : undefined}>contact</Link>
       </div>
+
+      <button className="hamburger-menu" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? <X /> : <Menu />}
+      </button>
 
       {/* light and dark mode */}
       {/* <div className={lightMode ? "app light-mode" : "app"}>
